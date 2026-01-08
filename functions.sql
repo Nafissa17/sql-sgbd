e5961f69ebcb3097633f0a75b34631362341f3bf
Mansour Djamil NDIAYE <mansourdjamil14@gmail.com>
2026-01-08 16:30:43 +0100

    commit

--- functions.sql
+++ functions.sql
@@ -3,90 +3,75 @@ GO
 
 /* =========================================================
    1. GET_NB_WORKERS
-   Retourne le nombre de travailleurs présents dans une usine
-   Paramètre : nom de l’usine
-   Vue utilisée : ALL_WORKERS
+   Retourne le nombre de travailleurs dans une usine
 ========================================================= */
-CREATE OR ALTER FUNCTION GET_NB_WORKERS(@factory_id INT)
+CREATE OR ALTER FUNCTION GET_NB_WORKERS(@factory_name VARCHAR(100))
 RETURNS INT
 AS
 BEGIN
-    RETURN (
-        SELECT COUNT(*)
-        FROM ALL_WORKERS aw
-        JOIN CONTRACTS c ON aw.worker_id = c.worker_id
-        WHERE c.factory_id = @factory_id
-    );
+    DECLARE @nb_workers INT;
+    
+    SELECT @nb_workers = COUNT(DISTINCT w.worker_id)
+    FROM ALL_WORKERS w
+    WHERE w.factory_name = @factory_name;
+    
+    RETURN ISNULL(@nb_workers, 0);
 END;
 GO
 
 /* =========================================================
    2. GET_NB_BIG_ROBOTS
    Compte le nombre de robots assemblés avec plus de 3 pièces
-   Vue utilisée : ROBOTS_FACTORIES
 ========================================================= */
-CREATE OR ALTER FUNCTION GET_NB_BIG_ROBOTS ()
+CREATE OR ALTER FUNCTION GET_NB_BIG_ROBOTS()
 RETURNS INT
 AS
 BEGIN
     DECLARE @nb_big_robots INT;
-
-    /*
-      Hypothèse métier :
-      - Un "big robot" = robot nécessitant > 3 types de pièces
-      - On passe par la vue ROBOTS_FACTORIES pour rester conforme
-    */
-
-    SELECT @nb_big_robots = COUNT(DISTINCT r.robot_id)
-    FROM ROBOTS r
-    JOIN ROBOT_PARTS rp ON r.robot_id = rp.robot_id
-    JOIN ROBOTS_FACTORIES rf ON rf.factory IS NOT NULL
-    GROUP BY r.robot_id
-    HAVING COUNT(rp.part_id) > 3;
-
+    
+    -- Compter les modèles de robot qui ont plus de 3 pièces différentes
+    -- Dans notre dataset, TOUS les modèles ont 7 pièces, donc > 3
+    SELECT @nb_big_robots = COUNT(DISTINCT rpc.model_id)
+    FROM ROBOT_PARTS_COMPOSITION rpc
+    GROUP BY rpc.model_id
+    HAVING COUNT(DISTINCT rpc.part_id) > 3;
+    
     RETURN ISNULL(@nb_big_robots, 0);
 END;
 GO
 
 /* =========================================================
    3. GET_BEST_SUPPLIER
-   Retourne le nom du fournisseur ayant livré le plus de pièces
-   Vue utilisée : BEST_SUPPLIERS
+   Retourne le nom du meilleur fournisseur
 ========================================================= */
-CREATE OR ALTER FUNCTION GET_BEST_SUPPLIER ()
+CREATE OR ALTER FUNCTION GET_BEST_SUPPLIER()
 RETURNS VARCHAR(100)
 AS
 BEGIN
     DECLARE @best_supplier VARCHAR(100);
-
-    SELECT TOP 1
-        @best_supplier = supplier
+    
+    SELECT TOP 1 @best_supplier = supplier
     FROM BEST_SUPPLIERS
     ORDER BY nb_parts DESC;
-
-    RETURN @best_supplier;
+    
+    RETURN ISNULL(@best_supplier, 'Aucun fournisseur');
 END;
 GO
 
 /* =========================================================
    4. GET_OLDEST_WORKER
    Retourne l'identifiant du travailleur le plus ancien
-   Vue utilisée : ALL_WORKERS
 ========================================================= */
-CREATE OR ALTER FUNCTION GET_OLDEST_WORKER ()
+CREATE OR ALTER FUNCTION GET_OLDEST_WORKER()
 RETURNS INT
 AS
 BEGIN
     DECLARE @oldest_worker_id INT;
-
-    SELECT TOP 1
-        @oldest_worker_id = w.worker_id
-    FROM ALL_WORKERS aw
-    JOIN WORKERS w
-        ON aw.lastname = w.lastname
-       AND aw.firstname = w.firstname
-    ORDER BY aw.start_date ASC;
-
-    RETURN @oldest_worker_id;
+    
+    SELECT TOP 1 @oldest_worker_id = worker_id
+    FROM ALL_WORKERS
+    ORDER BY start_date ASC;
+    
+    RETURN ISNULL(@oldest_worker_id, 0);
 END;
 GO
